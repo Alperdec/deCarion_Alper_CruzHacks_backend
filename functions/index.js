d8de13482ad2d7e378de8795963fb6d156f2cd2d
Alperdec <49699829+Alperdec@users.noreply.github.com>
2021-05-02 11:00:57 -0700

    initial commit
    asdasd

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,16 @@
 const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 const express = require('express');
+const { initializeApp } = require("firebase-admin");
+const { firebaseConfig } = require("firebase-functions");
 
+admin.initializeApp({credential: admin.credential.applicationDefault()})
 const db = admin.firestore();
-
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+    console.log('wowowow')
+})
+
+app.listen(3000)
