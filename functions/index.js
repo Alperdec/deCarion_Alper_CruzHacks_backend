5740350cdbad07b9422e34849fe0b3aeb606c44d
Alperdec <49699829+Alperdec@users.noreply.github.com>
2021-05-04 11:01:31 -0700

    major updates
    updated index.js to post to firebase

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,24 @@
 const functions = require("firebase-functions");
-const admin = require('firebase-admin');
-const express = require('express');
-const { initializeApp } = require("firebase-admin");
-const { firebaseConfig } = require("firebase-functions");
+const admin = require("firebase-admin");
+const express = require("express");
+const cors = require("cors");
 
-admin.initializeApp({credential: admin.credential.applicationDefault()})
-const db = admin.firestore();
+admin.initializeApp();
+// const db = admin.firestore();
 const app = express();
+app.use(cors({origin: true}));
 
-app.get('/', (req, res) => {
-    res.send('hello world')
-    console.log('wowowow')
-})
+app.post("/", async (req, res) => {
+  const user = req.body;
+  await admin.firestore().collection("users").add(user);
+  res.status(201).send();
+});
 
-app.listen(3000)
+app.post("/", async (req, res) => {
+  const application = req.body;
+  await admin.firestore().collection("applications").add(application);
+  res.status(201).send();
+});
+
+exports.user = functions.https.onRequest(app);
+exports.application = functions.https.onRequest(app);
