513f2ec08fca58cf87470a2a28f292fa4b6c0007
Alperdec <49699829+Alperdec@users.noreply.github.com>
2021-05-05 09:00:53 -0700

    more updates to index.js
    added endpoint to retrieve all applicants. added an endpoint for hacker s and volunteers

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,10 @@ const admin = require("firebase-admin");
 const express = require("express");
 const cors = require("cors");
 
+// index.js is a REST API with endpoints implemented 
+// using firebase functions and deployed to cloud
+// hence "serverless" implementation
+
 admin.initializeApp();
 const db = admin.firestore();
 // const db = admin.firestore();
@@ -10,29 +14,83 @@ const app = express();
 app.use(cors({origin: true}));
 
 app.get("/", async (req, res) => {
-  const response = await db.collection("general").get();
-  res.status(200).send(response);
+  // get all applicants
+  const snapshot1 = await db.collection("general")
+      .doc("applicants")
+      .collection("volunteers").get();
+  const snapshot2 = await db.collection("general")
+      .doc("applicants")
+      .collection("hackers").get();
+
+  const applicants = [];
+
+  snapshot1.forEach((doc) => {
+    const id = doc.id;
+    const data = doc.data();
+    applicants.push({id, ...data});
+  });
+  snapshot2.forEach((doc) => {
+    const id = doc.id;
+    const data = doc.data();
+    applicants.push({id, ...data});
+  });
+  res.status(200).send(JSON.stringify(applicants));
+});
+
+app.get("/hackers", async (req, res) => {
+  // get all hackers
+
+  const snapshot = await db.collection("general")
+      .doc("applicants")
+      .collection("hackers").get();
+
+  const hackers = [];
+
+  snapshot.forEach((doc) => {
+    const id = doc.id;
+    const data = doc.data();
+    hackers.push({id, ...data});
+  });
+
+  res.status(200).send(JSON.stringify(hackers));
+});
+
+app.get("/volunteers", async (req, res) => {
+  // get all volunteers
+
+  const snapshot = await db.collection("general")
+      .doc("applicants")
+      .collection("volunteers").get();
+
+  const volunteers = [];
+
+  snapshot.forEach((doc) => {
+    const id = doc.id;
+    const data = doc.data();
+    volunteers.push({id, ...data});
+  });
+
+  res.status(200).send(JSON.stringify(volunteers));
 });
 
 app.post("/", async (req, res) => {
-  const user =
-    {
-      firstname: req.body["firstname"],
-      lastname: req.body["lastname"],
-      gender: req.body["gender"],
-      email: req.body["email"],
-      age: req.body["age"],
-      applicationType: req.body["applicationType"],
-      resume: req.body["resume"],
-    };
-  await db.collection("general")
-      .doc("applicants")
-      .collection("volunteers")
-      .add(user);
-  res.status(201).send();
+  const applicant = req.body;
+  if (req.body["applicationType"] === "Volunteer") {
+    await db.collection("general")
+        .doc("applicants")
+        .collection("volunteers")
+        .add(applicant);
+    res.status(201).send();
+  } else if (req.body["applicationType"] === "Hacker") {
+    await db.collection("general")
+        .doc("applicants")
+        .collection("hackers")
+        .add(applicant);
+    res.status(201).send();
+  } else {
+    res.status(500).send("invalid application type");
+  }
 });
 
-
-exports.user = functions.https.onRequest(app);
-exports.response = functions.https.onRequest(app);
+exports.applicant = functions.https.onRequest(app);
 
